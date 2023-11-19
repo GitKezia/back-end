8fe20d3c71f1f3ae79680e33b3a277ecb64079c6
Kezia <keziavictoria21@gmail.com>
2023-11-19 19:26:38 -0300

    Tarefa Módulo 9

---
+++ Tipos complexos, boxing(autoboxing), unboxing e casting/Exemplo/src/App.java
@@ -0,0 +1,40 @@
+public class App {
+    public static void main(String[] args) {
+        
+        int i = 1;
+        short s = 1;
+        byte b = 4;
+        boolean o = true;
+        double d = 4;
+        float f = 4;
+        char c = 'a';
+        long l = 1;
+        
+        System.out.println(i);
+        System.out.println(s);
+        System.out.println(b);
+        System.out.println(o);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(c);
+        System.out.println(l);
+        
+        Integer x = new Integer(10);
+        Short ss = new Short((short)40);
+        Byte bb = new Byte((byte)5);
+        Boolean oo = new Boolean(false);
+        Double dd = new Double(4.5);
+        Float ff = new Float(3);
+        Character cc = new Character('h');
+        Long ll = new Long(70);
+        
+        System.out.println(x);
+        System.out.println(ss);
+        System.out.println(bb);
+        System.out.println(oo);
+        System.out.println(dd);
+        System.out.println(ff);
+        System.out.println(cc);
+        System.out.println(ll);
+    }
+}
